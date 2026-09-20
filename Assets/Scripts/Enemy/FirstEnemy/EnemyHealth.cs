0a9570c8bf4759e3ed42c69714248442ad561595
thuong <buiminhthuong30122005@gmail.com>
2026-09-20 17:03:52 +0700

    Add Chapter1 level and disable enemy FOV on death
    Adds a new Chapter1 level (Chapter1_Level1.unity) and folder meta, moves/renames the tutorial scene into Assets/Scenes/Levels/Chapter1 and updates EditorBuildSettings. FirstAI prefab adjusted (removed some components/references, cleared dropBulletCasingPoint, added fieldOfView2D reference). EnemyHealth.cs now exposes FieldOfView2D and disables it when the enemy dies. Small prefab instance/property cleanups included.

--- Assets/Scripts/Enemy/FirstEnemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/FirstEnemy/EnemyHealth.cs
@@ -5,6 +5,7 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] EnemyState enemyState;
     [SerializeField] EnemyShooting enemyShooting;
     [SerializeField] BoxCollider2D boxCollider2D;
+    [SerializeField] FieldOfView2D fieldOfView2D;
     public bool isDead;
 
     void Start()
@@ -19,6 +20,7 @@ public class EnemyHealth : MonoBehaviour
             enemyState.enabled = false;
             boxCollider2D.enabled = false;
             enemyShooting.enabled = false;
+            fieldOfView2D.enabled = false;
         }
     }
 }
